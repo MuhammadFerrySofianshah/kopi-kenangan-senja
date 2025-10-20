3230bfeeca7beb24231c16c6ecbb18db9664de4b
MuhammadFerrySofianshah <fsofian27@gmail.com>
2025-10-20 13:24:30 +0700

    update baru

--- js/script.js
+++ js/script.js
@@ -1,16 +1,97 @@
-// Toggle Class Active.
+//! 1. Toggle hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-
-// Ketika Humbuerger-menu di klik.
+// u/ ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Fungsi ketika mengklik diluar sidebar akan menghilangkan navbar
-const humburger = document.querySelector("#hamburger-menu");
+//! 2. Toggle search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+// u/ ketika search form di klik
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//! 3. Toggle shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+// u/ ketika shopping cart di klik
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+//! 4. Klik di luar elemen : humberger, search form, shopping cart
+const hm = document.querySelector("#hamburger-menu");
+const sf = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!humburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sf.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+//! 5. Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+// alasan menggunakan selectorAll karena class item-detail-button tidak hanya satu/lebih dari satu.
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+// u/ menampilkan detail modal
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+// u/ menghilangkan detail modal tanda x
+document.querySelector(".close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+// u/ klik di luar modal
+window.onclick = (e) => {
+  if (e.target == itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
+
+//! 6. alpine JS
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
+      { id: 2, name: "Arbica Blend", img: "2.jpg", price: 25000 },
+      { id: 3, name: "Priam Passa", img: "3.jpg", price: 30000 },
+      { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
+      { id: 5, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
+    ],
+  }));
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      this.items.push(newItem);
+      this.quantity++;
+      this.total += newItem.price;
+      console.log(this.total);
+    },
+  });
+});
+
+// ! 5. Conversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
